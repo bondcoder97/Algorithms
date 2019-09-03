bfabaa138f1375387df252b26d01bd801faee093
Andrey Bondarenko <andruha3097@gmail.com>
2019-09-03 23:09:26 +0300

    Twotrend and sorted lists

--- list.js
+++ list.js
@@ -105,4 +105,114 @@ class UnitrendList{
 
 
 
+//cell for two trend list
+class TwoTrendListCell extends Cell{
+  constructor(value="", Next=null, Previous=null){
+      super(value, Next);
+      this.Previous = Previous;
+  }
+}
+
+class TwoTrendList extends UnitrendList{
+  constructor(options={}){
+    super();
+    //bounders
+    let bottomBounder = new TwoTrendListCell("BOTTOM BOUNDER");
+    this.top = options.top || new TwoTrendListCell("TOP BOUNDER", bottomBounder);
+    bottomBounder.Previous = this.top;
+    this.bottom = bottomBounder;
+  }
+  
+  //METHOD OVERRIDING -------------------------------------------------------------------------------------------------
+
+  addToBegin(cell, top = this.top){
+      cell.Previous = top;
+      cell.Next = top.Next;
+      top.Next.Previous = cell;
+      top.Next = cell;
+  }
+
+  addToEnd(cell, bottom = this.bottom){
+      cell.Previous = bottom.Previous;
+      cell.Next = bottom;
+      bottom.Previous.Next = cell; 
+      bottom.Previous = cell;
+  }
+
+  addAfter(value, newCell){
+    try{
+      let afterMe = this.findCell(value);
+      if(!afterMe) throw new Error("Can not find needed element!");
+      newCell.Previous = afterMe;
+      newCell.Next = afterMe.Next;
+      afterMe.Next.Previous = newCell;
+      afterMe.Next = newCell;
+
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  addBefore(value, newCell){
+    try{
+       let beforeMe = this.findCell(value);
+       if(!beforeMe) throw new Error("Can not find needed element!");
+       newCell.Next = beforeMe;
+       newCell.Previous = beforeMe.Previous;
+       beforeMe.Previous.Next = newCell;
+       beforeMe.Previous = newCell;
+    }
+
+    catch(err){
+      console.log(err);
+    }
+
+  }
+
+  deleteElem(value){
+    try{
+      let deletedElem = this.findCell(value);
+      if(!deletedElem) throw new Error("Can not find deleted element!");
+      deletedElem.Previous.Next = deletedElem.Next;
+      deletedElem.Next.Previous = deletedElem.Previous;
+    
+    }
+    catch(err){
+       console.log(err);
+    }
+  }
+  //METHOD OVERRIDING : END ------------------------------------------------------------------------------------------
+
+}
+
+class SortedList extends TwoTrendList{
+  constructor(options={}){
+    super(options);
+      //bounders
+      let bottomBounder = new TwoTrendListCell("~");
+      this.top = options.top || new TwoTrendListCell("aaaaaaaaaaaa", bottomBounder); //top bounder
+      bottomBounder.Previous = this.top;
+      this.bottom = bottomBounder;
+  }
+
+  //sorted insert after element
+  cellInsert(newCell, top = this.top){
+      while(newCell.value < top.value){
+        top = top.Next;
+      }
+
+      newCell.Previous = top;
+      newCell.Next = top.Next;
+      top.Next.Previous = newCell;
+      top.Next = newCell;
+      
+      
+      
+  }
+
+  
+}
+
+
 
